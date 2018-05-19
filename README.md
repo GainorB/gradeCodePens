0a88ebd8ac66040ba8964ebcc096527442231914
GainorB <gainorbostwick@gmail.com>
2018-05-18 20:37:26 -0400

    done

---
+++ README.md
@@ -0,0 +1,52 @@
+## TO RUN
+
+1.  npm i or npm install dependencies
+
+## THEN
+
+In Terminal
+
+```javascript
+node main.js grade
+```
+
+```javascript
+node main.js g
+```
+
+or
+
+need to run: `npm link` to set up system link
+
+```javascript
+gradecodepens grade
+```
+
+## NEXT
+
+1.  Insert the class number (ex: 503)
+2.  Insert the name of the project you want to find. Case sensitive.
+3.  Open found projects in new tabs? (Y/N)
+
+## ./OUTPUT/
+
+This folder will contain .json files corresponding to the classes you searched for, in the following format: [CLASS NUMBER]-[DATE].json.
+
+Sample structure:
+
+```javascript
+   {
+      "link": "https://codepen.io/ANUR2021/pen/MGBEgM",
+      "student": "ANUR2021",
+      "screenshot": "http://codepen.io/ANUR2021/pen/MGBEgM/image/large.png"
+   },
+```
+
+## STATS
+
+1.  **NUMBER OF STUDENTS IN CLASS:** total number of scholars in class
+2.  **NUMBER OF FOUND PROJECTS:** total number of projects from that class that match the project you searched for
+3.  **NUMBER OF INCORRECTLY NAMED PROJECTS (NOT FOUND):** total number of scholars who incorrectly named the project
+4.  **STUDENTS WHO DIDN'T NAME PROJECT CORRECTLY:** names of scholars who didn't name project correctly
+5.  **NUMBER OF REJECTED REQUESTS:** total number of scholars who don't have any saved codePens
+6.  **REJECTED REQUESTS:** names of scholars who don't have any saved codePens
---
+++ main.js
@@ -0,0 +1,155 @@
+#!/usr/bin/env node
+
+const axios = require('axios');
+const fs = require('fs');
+const chalk = require('chalk');
+const { prompt } = require('inquirer');
+const program = require('commander');
+const moment = require('moment');
+const opn = require('opn');
+
+const { log } = console;
+
+// USED FOR CALLBACK
+let counter;
+// USED TO FILTER
+// THE NAME OF THE ASSIGNMENT YOU WANT TO GRAB FROM EACH STUDENTS CODEPENS
+let theKeyword;
+
+// ARRAY OF QUESTIONS TO ASK USER
+const questions = [
+  { type: 'input', name: 'classNumber', message: 'Which class (#)?' },
+  { type: 'input', name: 'keyword', message: 'Name of the project?' },
+  { type: 'input', name: 'openTabs', message: 'Open found projects in new tabs? (Y/N)' },
+];
+
+// OBJECT CONTAINING ARRAYS OF STUDENTS
+const allStudents = {
+  501: require('./students/501.json'),
+  502: require('./students/502.json'),
+  503: require('./students/503.json'),
+  601: require('./students/601.json'),
+  602: require('./students/602.json'),
+  603: require('./students/603.json'),
+  701: require('./students/701.json'),
+  702: require('./students/702.json'),
+  703: require('./students/703.json'),
+  801: require('./students/801.json'),
+  802: require('./students/802.json'),
+  803: require('./students/803.json'),
+};
+
+// TODO: Get HTML/CSS content from codePen
+// TODO: Possibly connect to Remind API (if one exists) to automatically alert parents, etc..
+// TODO: Possibly export to Excel or Google Sheets?
+// TODO: Possibly..
+
+// IF PROJECT IS FOUND INSERT INTO THIS ARRAY
+const FOUNDPROJECTS = [];
+
+// NUMBER OF FAILED PROJECTS
+let FAILEDPROJECTS = 0;
+const FAILEDSTUDENTS = [];
+
+// NUMBER OF REJECTED REQUESTS (STUDENTS)
+let REJECTED = 0;
+const REJECTEDSTUDENTS = [];
+
+function date() {
+  return moment().format('MMMM Do YYYY h:mm');
+}
+
+// PARSE AND FILTER DATA FROM API
+function codePen({ data, username, openTabs }) {
+  // FILTER ARRAY OF PENS TO GET SPECIFIC PROJECT
+  if (data !== undefined) {
+    const project = data.filter(pen => pen.title === theKeyword).map(pen => ({
+      link: pen.link,
+      student: pen.user.username,
+      screenshot: pen.images.large,
+    }))[0];
+
+    if (project !== undefined) {
+      FOUNDPROJECTS.push(project);
+      if (openTabs.toLowerCase() === 'y') {
+        // OPEN PROJECT LINK IN NEW TAB IN DEFAULT BROWSER
+        opn(project.link, { wait: false });
+      }
+    } else {
+      FAILEDPROJECTS += 1;
+      FAILEDSTUDENTS.push(username);
+    }
+  } else {
+    REJECTED += 1;
+    REJECTEDSTUDENTS.push(username);
+  }
+}
+
+// STATS PRINTED TO CONSOLE
+function stats(students, classNumber) {
+  log(chalk.green.bgBlack(`------------------------------`));
+  log(chalk.green.bgBlack(`STATS`));
+  log(chalk.green.bgBlack(`------------------------------`));
+  log(`# OF STUDENTS IN CLASS: `, students.length);
+  log(`# OF FOUND PROJECTS: `, FOUNDPROJECTS.length);
+  if (FAILEDPROJECTS > 0) {
+    log(`# OF INCORRECTLY NAMED PROJECTS (NOT FOUND): `, FAILEDPROJECTS);
+    log(`STUDENTS WHO DIDN'T NAME PROJECT CORRECTLY: `, FAILEDSTUDENTS);
+  }
+  if (REJECTED > 0) {
+    log(`# OF REJECTED REQUESTS: `, REJECTED);
+    log(`REJECTED REQUESTS: `, REJECTEDSTUDENTS);
+  }
+  log(chalk.green.bgBlack(`------------------------------`));
+
+  // CREATE OUTPUT FOLDER IF IT DOESN'T EXIST
+  fs.existsSync('output') || fs.mkdirSync('output');
+  // CREATE FILE
+  fs.writeFile(`./output/${classNumber}-${date()}.json`, JSON.stringify(FOUNDPROJECTS, null, 3), err => {
+    if (err) throw err;
+    console.log('File created!');
+  });
+}
+
+// GO
+function go({ classNumber, keyword, openTabs }) {
+  // SELECTED CLASS
+  const students = allStudents[classNumber];
+  // DETERMINES WHEN THE CALLBACK IS CALLED AFTER THE FOREACH IS COMPLETED
+  counter = students.length;
+
+  // KEYWORD TO SEARCH FOR
+  theKeyword = keyword;
+
+  // LOOP THROUGH STUDENTS AND HIT API
+  students.forEach(name => {
+    axios
+      .get(`https://cpv2api.com/pens/public/${name}`, {
+        timeout: 10000,
+      })
+      .then(res => {
+        const username = res.request._redirectable._options.pathname.split('/')[3];
+        codePen({ data: res.data.data, username, openTabs });
+        counter -= 1;
+        // CALLBACK IS FIRED WHEN COUNTER HITS ZERO
+        if (counter === 0) {
+          stats(students, classNumber);
+        }
+      })
+      .catch(err => log(err));
+  });
+}
+
+program.version('1.0.0').description('Check students Capstone Projects in CodePen');
+
+program
+  .command('grade')
+  .alias('g')
+  .description('Check a class for the assignment')
+  .action(() => {
+    prompt(questions).then(answers => {
+      go(answers);
+    });
+  });
+
+program.parse(process.argv);
